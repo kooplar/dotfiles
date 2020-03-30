ce6cfb372faf342b240d3f4a62361275890f0546
Bryan Martin <kooplar@gmail.com>
2020-03-29 21:24:46 -0400

    test files

---
+++ test_files/a.py
@@ -0,0 +1,23 @@
+import os
+u
+def a():
+    if True:
+        return 7
+    a = {
+        1: 2
+        1: 2,
+        1: 2,
+        1: 2,
+        1: 2,
+        1: 2,
+        1: 2,
+        1: 2,
+        1: 2,
+        1: 2,
+        1: 2,
+        3: 4
+    }
+def bye():
+    pass
+def hello():
+    pass
